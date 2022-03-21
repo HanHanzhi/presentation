f60e774117564f1e47629153e9b8ab94c35b9f7a
GianniGabriel <giannigab789@gmail.com>
2022-03-21 16:38:36 +0800

    Add Global GetRecipes Function

--- src/context/recipes/RecipeReducer.js
+++ src/context/recipes/RecipeReducer.js
@@ -2,6 +2,11 @@ import { GET_RECIPES } from "../types";
 
 const recipeReducer = (state, action) => {
   switch (action.type) {
+    case GET_RECIPES:
+      return {
+        ...state,
+        recipes: action.payload,
+      };
     default:
       return state;
   }
--- src/context/recipes/RecipeState.js
+++ src/context/recipes/RecipeState.js
@@ -1,19 +1,40 @@
 import React, { useReducer } from "react";
+import axios from "axios";
 import RecipeContext from "./RecipeContext";
 import RecipeReducer from "./RecipeReducer";
 import { GET_RECIPES } from "../types";
 
 const RecipeState = (props) => {
+  const SPOONACULAR_URI = "https://api.spoonacular.com";
+  const QUERY_CONSTANTS = {
+    RESPONSE_NUM: 5,
+    RANKING: 1, // Maximize used ingredients (1) or minimize missing ingredients (2)
+    IGNORE_PANTRY: true,
+  };
   const initialState = {
     recipes: [],
   };
 
   const [state, dispatch] = useReducer(RecipeReducer, initialState);
 
+  // Get recipes by ingredient list
+  const getRecipes = async (ingredients) => {
+    const ingredientsStr = ingredients.join();
+    const getRecipeURI = `${SPOONACULAR_URI}/recipes/findByIngredients?ingredients=${ingredientsStr}&number=${QUERY_CONSTANTS.RESPONSE_NUM}&ranking=${QUERY_CONSTANTS.RANKING}&ignorePantry=${QUERY_CONSTANTS.IGNORE_PANTRY}`;
+
+    try {
+      const res = await axios.get(getRecipeURI);
+      dispatch({ type: GET_RECIPES, payload: res.data });
+    } catch (err) {
+      console.error(err.response);
+    }
+  };
+
   return (
     <RecipeContext.Provider
       value={{
         recipes: state.recipes,
+        getRecipes,
       }}
     >
       {props.children}

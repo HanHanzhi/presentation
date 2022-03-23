3454a7858877a7651d4b9c1ed3bbf826557e8305
GianniGabriel <giannigab789@gmail.com>
2022-03-23 17:36:14 +0800

    Add typo to search button

--- src/components/layout/Search/SearchArea.js
+++ src/components/layout/Search/SearchArea.js
@@ -28,7 +28,7 @@ const SearchArea = () => {
         setIngredientList={setIngredientList}
       />
       <button onClick={onSearch} className={searchBtn}>
-        Search
+        Serchh
       </button>
     </div>
   );
